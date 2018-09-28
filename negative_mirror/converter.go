0a25d17f9c6134e89f8a66c3d80c1b9ce8c5284c
Karan Sivrat <bomb0069@gmail.com>
2018-09-29 00:48:48 +0700

    solved: Two member in only one rows

--- negative_mirror/converter.go
+++ negative_mirror/converter.go
@@ -8,5 +8,14 @@ func NewConverter() Converter {
 }
 
 func (c Converter) NegativeMirrorOf(original [][]bool) [][]bool {
-	return original
+	negativeMirror := make([][]bool, len(original))
+	for outerIndex, innerArray := range original {
+		innerSize := len(innerArray)
+		nagativeMirrorRow := make([]bool, innerSize)
+		for innerIndex, value := range innerArray {
+			nagativeMirrorRow[(innerSize-1)-innerIndex] = !value
+		}
+		negativeMirror[outerIndex] = nagativeMirrorRow
+	}
+	return negativeMirror
 }
--- negative_mirror/nagative_mirror_test.go
+++ negative_mirror/nagative_mirror_test.go
@@ -17,3 +17,29 @@ func Test_empty_array(t *testing.T) {
 		t.Errorf("%v != %v", expected, actual)
 	}
 }
+
+func Test_only_one_member_it_mirror(t *testing.T) {
+	original := [][]bool{{true}}
+	expected := [][]bool{{false}}
+
+	converter := NewConverter()
+
+	actual := converter.NegativeMirrorOf(original)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("%v != %v", expected, actual)
+	}
+}
+
+func Test_two_member_one_row_it_mirror(t *testing.T) {
+	original := [][]bool{{true, false}}
+	expected := [][]bool{{true, false}}
+
+	converter := NewConverter()
+
+	actual := converter.NegativeMirrorOf(original)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("%v != %v", expected, actual)
+	}
+}
